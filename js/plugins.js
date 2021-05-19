ed0eca8dcec8202e83bc923c322af03cf87baf8e
John Kearney <John.d.kearney1@gmail.com>
2021-05-19 16:06:54 -0500

    want to see how the form handles included validation

--- js/plugins.js
+++ js/plugins.js
@@ -217,50 +217,50 @@ $(document).ready(function () {
 
 /*========== CONTACT FORM INPUT VALIDATION ==========*/
 //Original Resource: https://bootstrapious.com/p/how-to-build-a-working-bootstrap-contact-form
-// $(function () {
+$(function () {
 
-//     // init the validator
-//     // validator files are included in the download package
-//     // otherwise download from http://1000hz.github.io/bootstrap-validator
+    // init the validator
+    // validator files are included in the download package
+    // otherwise download from http://1000hz.github.io/bootstrap-validator
   
-//     $('#contact-form').validator();
+    $('#contact-form').validator();
   
   
-//     // when the form is submitted
-//     $('#contact-form').on('submit', function (e) {
-//     //   console.log('Contact form submit clicked.');
-//         // if the validator does not prevent form submit
-//         if (!e.isDefaultPrevented()) {
-//             var url = "contact/gmail-contact.php";
+    // when the form is submitted
+    $('#contact-form').on('submit', function (e) {
+    //   console.log('Contact form submit clicked.');
+        // if the validator does not prevent form submit
+        // if (!e.isDefaultPrevented()) {
+        //     var url = "contact/gmail-contact.php";
   
-//             // POST values in the background the the script URL
-//             $.ajax({
-//                 type: "POST",
-//                 url: url,
-//                 data: $(this).serialize(),
-//                 success: function (data) {
-//                     // data = JSON object that contact.php returns
+        //     // POST values in the background the the script URL
+        //     $.ajax({
+        //         type: "POST",
+        //         url: url,
+        //         data: $(this).serialize(),
+        //         success: function (data) {
+        //             // data = JSON object that contact.php returns
   
-//                     // we recieve the type of the message: success x danger and apply it to the
-//                     var messageAlert = 'alert-' + data.type;
-//                     var messageText = data.message;
+        //             // we recieve the type of the message: success x danger and apply it to the
+        //             var messageAlert = 'alert-' + data.type;
+        //             var messageText = data.message;
   
-//                     // let's compose Bootstrap alert box HTML
-//                     var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+        //             // let's compose Bootstrap alert box HTML
+        //             var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
   
-//                     // If we have messageAlert and messageText
-//                     if (messageAlert && messageText) {
-//                         // inject the alert to .messages div in our form
-//                         $('#contact-form').find('.messages').html(alertBox);
-//                         // empty the form
-//                         $('#contact-form')[0].reset();
-//                     }
-//                 }
-//             });
-//             return false;
-//         }
-//     })
-//   });
+        //             // If we have messageAlert and messageText
+        //             if (messageAlert && messageText) {
+        //                 // inject the alert to .messages div in our form
+        //                 $('#contact-form').find('.messages').html(alertBox);
+        //                 // empty the form
+        //                 $('#contact-form')[0].reset();
+        //             }
+        //         }
+        //     });
+        //     return false;
+        // }
+    })
+  });
 
 /*========== WAYPOINTS ANIMATION DELAY ==========*/
 //Original Resource: https://www.oxygenna.com/tutorials/scroll-animations-using-waypoints-js-animate-css
